59a8efa272b7dd486f3edd5e2a7c46fca7352ce2
HairDuong <101450768+HairDuong@users.noreply.github.com>
2022-03-12 15:35:31 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " Nguyen Hai Duong" << endl;
+    cout << " mssv: 106210028" << endl;
+    cout << " lop : 21DTCLC1";
+    return 0;
+}
